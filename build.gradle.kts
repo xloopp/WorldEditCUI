9100b7c2bdc2b1d48c1681a7522904c1c61d7153
xloopp <1991315247@qq.com>
2025-05-01 15:23:21 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,11 @@ allprojects {
         }
     }
 }
-
+dependencies {
+    mappings("org.parchmentmc.data:parchment-1.21.4:2024.12.07@zip") {
+        artifactUrls("https://maven.parchmentmc.org/org/parchmentmc/data/parchment-1.21.4/2024.12.07/")
+    }
+}
 subprojects {
     apply(plugin = "java")
     apply(plugin = "com.diffplug.spotless")
